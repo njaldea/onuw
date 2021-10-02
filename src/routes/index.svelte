fc0ce58bf325a3b4eec2d3d7c5f893a00d04f1b0
Neil Aldea <njaldea@gmail.com>
2021-10-02 11:27:31 +0800

    clean up

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class='root'>
+    <div>
+    </div>
+</div>
+
+<style>
+    div > div {
+        width: 50%;
+        height: 50%;
+        background-color: red;
+    }
+
+    .root {
+        width: 100%;
+        height: 100%;
+        display: grid;
+        place-items: center;
+    }
+
+    :global(*, *::before, *::after) {
+        box-sizing: border-box;
+        margin: 0;
+        border: 0;
+    }
+
+    :global(html, body, #svelte) {
+        width: 100%;
+        height: 100%;
+    }
+</style>
