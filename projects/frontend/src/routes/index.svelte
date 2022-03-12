2931271f1f1cc6816e846a391c7c09690f6ef198
Neil Aldea <njaldea@gmail.com>
2022-03-12 18:45:10 -0500

    babylonjs trial

---
+++ projects/frontend/src/routes/index.svelte
@@ -0,0 +1,21 @@
+<section>
+    <a href="/3D">3D</a>
+    <a href="/chess">chess</a>
+    <a href="/diagrams">diagrams</a>
+</section>
+
+<svelte:head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/firacode@6.2.0/distr/fira_code.css" />
+</svelte:head>
+
+<style>
+    section {
+        flex-direction: column;
+        font-family: 'Fira Code', 'Courier New', Courier, monospace;
+        padding: 2rem;
+    }
+
+    a {
+        color: white;
+    }
+</style>
